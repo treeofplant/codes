2aeaea2b7ca8fb8c85bd601d30cd26a30147b76b
hu <77543293+treeofplant@users.noreply.github.com>
2024-06-15 02:05:59 +0400

    Update gymleague.lua

--- gymleague.lua
+++ gymleague.lua
@@ -1,4 +1,8 @@
 local codes = {"5KLikes", "10KLikes", "1MVisits", "SORRY"}
 for i,v, in pairs(codes) do
-game:GetService("ReplicatedStorage"):WaitForChild("common"):WaitForChild("packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("CodeService"):WaitForChild("RF"):WaitForChild("Redeem"):InvokeServer(v)
+local args = {
+    [1] = v
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("common"):WaitForChild("packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("CodeService"):WaitForChild("RF"):WaitForChild("Redeem"):InvokeServer(unpack(args))
 end
